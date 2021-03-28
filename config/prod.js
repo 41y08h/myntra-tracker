6631d5383e2347bc814a8e42df87f022c59bad9f
Piyush <piyush41y08h@gmail.com>
2021-03-28 12:50:18 +0530

    Change track interval

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
   scraperApiKey: process.env.SCRAPER_API_KEY,
   receiverEmail: process.env.RECEIVER_EMAIL,
   receiverName: process.env.RECEIVER_NAME,
-  trackInterval: 1000 * 60 * 2,
+  trackInterval: 1000 * 60 * 10,
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   refreshToken: process.env.REFRESH_TOKEN,
